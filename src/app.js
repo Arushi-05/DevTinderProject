94f7ef7987260fa6131dff9b631a2e1be6822bcb
Arushi Kapoor <me.arushikapoor@gmail.com>
2025-11-11 07:46:37 +0530

    adding post user API

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 const express = require('express')
-
+const User = require('./models/user')
 //const authAdmin=require('./middlewares/adminAuth')
 const app = express()
 const PORT = 8000
-const connectDB=require('./config/dataBase')
+const connectDB = require('./config/dataBase')
+app.use(express.json())
+app.post("/signup", async (req, res) => {
+   
+    const user = new User(req.body)
+    try {
+        await user.save();
+        res.send("New user added successfully.")
+    }
+    catch (err) {
+        res.status(400).send(`Error saving the new user:${err.message}`)
+    }
+})
 
-
-
-
-connectDB().then(()=>{
+connectDB().then(() => {
     console.log("DB connected!")
     app.listen(PORT, () => {
         console.log(`Hi server port ${PORT}.`)
     });
-}).catch(err=>{
+}).catch(err => {
     console.log(`DB not connected yet because of ${err}.`)
 })
