86d4ced064eff48ab678f8b018333af71e463fd3
Arushi Kapoor <me.arushikapoor@gmail.com>
2025-11-12 08:25:56 +0530

    more validations with validator added

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
-
+const validator=require("validator")
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     firstName: { type: String, required: true, trim: true, max: 50 },
     lastName: { type: String, max: 50, trim: true },
-    emailId: { type: String, immutable: true, required: true, unique: true, trim: true, lowercase: true },
+    emailId: { type: String, immutable: true, required: true, unique: true, trim: true, lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Please enter a validate email address.")
+            }
+        }
+     },
     password: { type: String, required: true, trim: true },
     age: { type: Number, min: 18 },
     gender: {
